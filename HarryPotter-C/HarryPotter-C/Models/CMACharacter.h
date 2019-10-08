a3c5811849f085c2dfd73b98f6ac0622f6179c51
Chris Alegre <calegre14@gmail.com>
2019-10-08 12:04:24 -0600

    Finished Model

--- HarryPotter-C/HarryPotter-C/Models/CMACharacter.h
+++ HarryPotter-C/HarryPotter-C/Models/CMACharacter.h
@@ -12,6 +12,24 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface CMACharacter : NSObject
 
+@property(nonatomic, copy, readonly)NSString *name;
+@property(nonatomic, copy, readonly, nullable)NSString *house;
+@property(nonatomic, copy, readonly)NSString *bloodStatus;
+@property(nonatomic, readonly)BOOL deathEater;
+@property(nonatomic, copy, readonly)NSString *role;
+
+-(instancetype)initWithCharacterName:(NSString *)name
+                               house:(NSString *)house
+                          bloodSatus:(NSString *)bloodStatus
+                          deathEater:(BOOL)deathEater
+                                role:(NSString *)role;
+
+@end
+
+@interface CMACharacter (JSONConvertable)
+
+- (instancetype)initWithDictionary:(NSDictionary<NSString *, id> *)dictionary;
+
 @end
 
 NS_ASSUME_NONNULL_END
--- HarryPotter-C/HarryPotter-C/Models/CMACharacter.m
+++ HarryPotter-C/HarryPotter-C/Models/CMACharacter.m
@@ -8,6 +8,42 @@
 
 #import "CMACharacter.h"
 
+static NSString * const kName = @"name";
+static NSString * const kHouse = @"house";
+static NSString * const kBloodStatus = @"bloodStatus";
+static NSString * const kDeathEater = @"deathEater";
+static NSString * const kRole = @"role";
+
 @implementation CMACharacter
+
+- (instancetype)initWithCharacterName:(NSString *)name house:(NSString *)house bloodSatus:(NSString *)bloodStatus deathEater:(BOOL)deathEater role:(NSString *)role
+{
+    if (self = [super init])
+        //self = [super init]
+        //if (self)
+    {
+        _name = name;
+        _house = house;
+        _bloodStatus = bloodStatus;
+        _deathEater = deathEater;
+        _role = role;
+    }
+    return self;
+}
+
+@end
+
+@implementation CMACharacter (JSONConvertable)
+
+- (instancetype)initWithDictionary:(NSDictionary<NSString *,id> *)dictionary
+{
+    NSString *name = dictionary[kName];
+    NSString *house = dictionary[kHouse];
+    NSString *bloodStatus = dictionary[kBloodStatus];
+    BOOL deathEater = dictionary[kDeathEater];
+    NSString *role = dictionary[kRole];
+    
+    return [self initWithCharacterName:name house:house bloodSatus:bloodStatus deathEater:deathEater role:role];
+}
 
 @end
